4461a00597c892c24f30df0d2965eaa133377da8
Dimitri Miranda <164142097+Dimitri-Miranda@users.noreply.github.com>
2025-05-18 05:35:24 -0300

    Update helpers.py
    Fix explorer opening triggering false error

--- helpers.py
+++ helpers.py
@@ -3,15 +3,23 @@ from config import load_config
 
 DESTINY_DIR, FILE_EXPLORER, FILE_ARCHIVER = load_config()
 
+def log_info(msg): return (f"\033[35m[INFO]\033[0m {msg}")
+
+def log_ok(msg): print(f"\033[92m[OK]\033[0m {msg}")
+
+def log_warn(msg): print(f"\033[93m[WARN]\033[0m {msg}")
+
+def log_error(msg): print(f"\033[91m[ERRO]\033[0m {msg}")
+
 def get_bak_files_names():
     if not os.path.exists(DESTINY_DIR):
-        print("[ERRO] Diretorio não existe")
+        log_error("Diretorio não existe")
         return False
     
     files = os.listdir(DESTINY_DIR)
 
     if not files:
-        print("[WARN] Diretorio vazio")
+        log_warn("Diretorio vazio")
         return False
 
     files_dict = {}
@@ -26,13 +34,13 @@ def clean_baks_directory():
     except FileNotFoundError:
         pass
     except Exception as e:
-        print(f"[ERRO] Falha ao remover o diretorio: {e}")
+        log_error(f"Falha ao remover o diretorio: {e}")
         return False
 
     try:
         os.makedirs(DESTINY_DIR, exist_ok=True)
     except Exception as e:
-        print(f"[ERRO] Falha ao criar o diretorio: {e}")
+        log_error(f"Falha ao criar o diretorio: {e}")
         return False
 
     return True
@@ -40,14 +48,6 @@ def clean_baks_directory():
 def open_backup_dir():
     if os.path.exists(DESTINY_DIR):
         try:
-            subprocess.run([FILE_EXPLORER, DESTINY_DIR], check=True)
+            subprocess.run([FILE_EXPLORER, DESTINY_DIR])
         except subprocess.CalledProcessError as e:
-            print(f"[ERRO] Falha ao abrir o {FILE_EXPLORER}:", e)
-
-def log_info(msg): return (f"\033[35m[INFO]\033[0m {msg}")
-
-def log_ok(msg): print(f"\033[92m[OK]\033[0m {msg}")
-
-def log_warn(msg): print(f"\033[93m[WARN]\033[0m {msg}")
-
-def log_error(msg): print(f"\033[91m[ERRO]\033[0m {msg}")
+            log_error(f"Falha ao abrir o {FILE_EXPLORER}: {e}")
