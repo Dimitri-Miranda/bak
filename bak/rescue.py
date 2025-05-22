c25cfddb2029a198afef22ab6f1d87cb2238cf7e
Dimitri-Miranda <Contato.Dimitri@outlook.com>
2025-05-22 11:11:26 -0300

    Update rescue.py

--- bak/rescue.py
+++ bak/rescue.py
@@ -1,15 +1,15 @@
-import os, shutil, subprocess
+import os, shutil
 from .config import load_config
 from .logger import log_ok, log_warn, log_error
-from .helpers import get_bak_files_names 
+from .helpers import get_bak_files_names, search_fd, file_per_index
 
 DESTINY_DIR, FILE_EXPLORER, FILE_ARCHIVER = load_config()
 
 def rescue_file(file_path):
     file_dict = get_bak_files_names()
 
-    if file_dict and file_path in file_dict:
-        file_path = file_dict[file_path]
+    # vai pegar o nome do arquivo caso tenha sido passado por index
+    file_path = file_per_index(file_dict, file_path)
 
     dir_cwd = os.getcwd()
 
@@ -38,19 +38,6 @@ def rescue_multiples(file_list):
 
     return rescue_file_list
 
-def search_fd(command):
-    try:
-        results = subprocess.run(["fd", command, DESTINY_DIR], capture_output=True, text=True, check=True)
-        files = results.stdout.strip().split("\n")
-
-        file_path = os.path.basename(files[0]) if files else None
-
-        return file_path
-
-    except subprocess.CalledProcessError as e:
-        log_error(f"Falha ao rodar comando fd: {e}")
-        return None
-
 def handle_rescue(args_file=None, args_search=None):
 
     if args_file:
@@ -60,7 +47,7 @@ def handle_rescue(args_file=None, args_search=None):
 
     if args_search:
         for file in args_search:
-            fd_file = search_fd(file)
+            fd_file = search_fd(file, DESTINY_DIR)
 
             if fd_file:
                 try:
